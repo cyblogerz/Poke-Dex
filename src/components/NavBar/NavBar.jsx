31fb92a47f1a560b14695800d48931adabf5e281
Pranav Ajay <pranavajay2002@gmail.com>
2022-01-28 11:02:38 +0530

    Modified navbar component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,13 +5,11 @@ export default function NavBar(){
     return(
         <div className="NavBar">
         <h1 className="title">PokeDex</h1>
-        <div className="links">
-        <a href="/" className="nav-link">Home</a>
-        <a href="/" className="nav-link">About</a>
-        <a href="/" className="nav-link">Pokemon</a>
-        </div>
-        
-
+        <ul className="links">
+            <li className="nav-link"><a href="/">About</a></li>
+            <li className="nav-link"><a href="/">Pokemon </a> </li>
+            <li className="className"><a href="/">Home</a></li>
+        </ul>
         </div>
     );
 }
