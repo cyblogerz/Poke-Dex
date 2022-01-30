be9dc58e199b3be41c27c7127bbe217ce282ac92
Pranav Ajay <pranavajay2002@gmail.com>
2022-01-30 18:44:09 +0530

    Update pokeCard.jsx

--- src/components/pokeCard.jsx
+++ src/components/pokeCard.jsx
@@ -11,11 +11,24 @@ function PokeCard(props) {
       transform: 'scale(1.05)'
     }}
     
-    boxShadow={'1px 1px 10px cyan,1px 1px 10px cyan,1px 1px 10px cyan,1px 1px 40px cyan;'}
+    boxShadow={'1px 1px 10px cyan,1px 1px 10px cyan,1px 1px 20px cyan,1px 1px 40px cyan;'}
      flexDirection={'column'}  borderRadius={'10px'}  p={props.isCompact ?'30px' : '20px' }   >
     <Image 
-    // boxSize={props.isNotTablet ? null : '300px'}
-    boxSize={'70%'}
+    //  boxSize={'70%'}
+    
+    // mx={'auto'}
+    // // m={!props.isCompact ? (props.isNotTablet ? '10px' : '30px') : '20px'}
+    // // m={'20px/'}
+    
+    height={!props.isNotTablet ? '100%' : null}
+    width={!props.isNotTablet ? '100%' : null}
+    
+    // pb={'30px'}
+    // src={props.url} 
+    // fit={'contain'}
+    // borderRadius={'10px'}
+    boxSize={!props.isCompact ? '70%' : null}
+    // boxSize={'70%'}
     mx={'auto'}
     // m={!props.isCompact ? (props.isNotTablet ? '10px' : '30px') : '20px'}
     // m={'20px/'}
