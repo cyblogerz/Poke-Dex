aee67214264857a0d75d1bc530eef4a1e2ceadc7
Pranav Ajay <pranavajay2002@gmail.com>
2022-01-30 17:53:47 +0530

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import SearchInput from './SearchInput';
 
 function Welcome(props) {
   return <Box bg={'#1a212d'} borderRadius={'10px'} position={'absolute'} p={'20px'}
-  boxShadow={'1px 1px 10px cyan,1px 1px 10px cyan,1px 1px 20px cyan,1px 1px 40px cyan;'}
+  boxShadow={'1px 1px 10px cyan,1px 1px 10px purple,1px 1px 20px cyan,1px 1px 80px purple;'}
 
      left={'0'} right={'0'}
      top={'18%'}
@@ -13,10 +13,11 @@ function Welcome(props) {
   >
     <Flex direction={'column'} pb={'3rem'} >
       <Heading size={'3xl'} textAlign={'center'} pb={'15px'}
-       fontWeight={'400'}
+       fontWeight={'400'} bgGradient={'linear(to-r,purple,blue,)'} bgClip={'text'}      
       
       >PokéDex</Heading>
-      <Text textAlign={'center'}>A Hobby project by @cyblogerz</Text>
+      <Text textAlign={'center'} bgGradient={'linear(to-r,cyan.400,blue.500,purple.600)'} bgClip={'text'}
+      fontWeight={'400'}>A Hobby project by @cyblogerz</Text>
       </Flex>
      
       <SearchInput />
